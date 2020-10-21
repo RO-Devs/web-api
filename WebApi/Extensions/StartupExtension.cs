211eb30d52d316abdc3b2c904080022c3dc48a90
ORBIS ALONZO GUTIERREZ <orbis_ag@hotmail.com>
2020-10-20 20:30:24 -0400

    init

---
+++ WebApi/Extensions/StartupExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Extensions
+{
+    public static class StartupExtension
+    {
+        public static void ServicesImplementations(this IServiceCollection services)
+        {
+        }
+    }
+}
